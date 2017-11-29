ba764a275ecbbf684409da19d69181cc01a1e567
Pierre DELAUNAY <delasalle.sio.delaunay.pierre@gmail.com>
2017-11-29 22:54:05 +0100

    Update controleur - Pierre 29/11/2017 22:54

--- controleurs/CtrlContact.php
+++ controleurs/CtrlContact.php
@@ -12,9 +12,9 @@ if (isset($_POST['mail']) && isset($_POST['sujet']) && isset($_POST['contenu']))
     
     $mail = $_POST['mail'];
     $sujet = $_POST['sujet'];
-    $contenu = $_POST['contenu'];
-    $mail_emetteur = "delasalle.sio.eleves@gmail.com";
-    $mail_destinataire = "delasalle.sio.delaunay.pierre@gmail.com";
+    $contenu = 'Nouveau message de '.$mail.' Contenu du message : '.$_POST['contenu'];
+    $mail_emetteur = "dls@nexart.fr";
+    $mail_destinataire = "dls@nexart.fr";
 
     $ok = Outils::envoyerMail($mail_destinataire, $sujet, $contenu, $mail_emetteur);
     
